8d553fa580ed1d536f96ee83dd6207b5af0a302d
Grazielli Lima Berti <limaberti.grazi@gmail.com>
2023-06-19 18:29:34 -0300

    create app CestasDaCica

---
+++ CestaDaCica/CestaDaCica/CestaDaCicaApp.swift
@@ -0,0 +1,17 @@
+//
+//  CestaDaCicaApp.swift
+//  CestaDaCica
+//
+//  Created by Grazi  Berti on 19/06/23.
+//
+
+import SwiftUI
+
+@main
+struct CestaDaCicaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CestaDaCica/CestaDaCica/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  CestaDaCica
+//
+//  Created by Grazi  Berti on 19/06/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
