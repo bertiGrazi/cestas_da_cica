1bfc199ea877b2005f46f920e5720908b2d506ca
Grazielli Lima Berti <limaberti.grazi@gmail.com>
2023-06-19 20:03:40 -0300

    create model and add lottie json

---
+++ CestaDaCica/CestaDaCica/Model/OnBoardingItem.swift
@@ -0,0 +1,17 @@
+//
+//  OnBoardingItem.swift
+//  CestaDaCica
+//
+//  Created by Grazi  Berti on 19/06/23.
+//
+
+import Foundation
+import Lottie
+
+// MARK: - OnBoarding Item Model
+struct OnBoardingItem: Identifiable, Equatable {
+    var id: UUID = .init()
+    var title: String
+    var subtitle: String
+    var lottieView: LottieView = .init()
+}
